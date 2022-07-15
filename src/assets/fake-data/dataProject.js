a9ee8344fcbe4d058ae1bbef8c3dc1a1dc9883f7
Creative Social NFT <dev@creativesocialnft.com>
2022-07-15 19:29:25 -0400

    Initial file uploads

---
+++ src/assets/fake-data/dataProject.js
@@ -0,0 +1,139 @@
+import img1 from '../images/common/img1.jpg'
+import img2 from '../images/common/img2.jpg'
+import img3 from '../images/common/img3.jpg'
+import img4 from '../images/common/img4.jpg'
+import img5 from '../images/common/img5.jpg'
+import img6 from '../images/common/img6.jpg'
+import img7 from '../images/common/img7.jpg'
+import img9 from '../images/common/img9.jpg'
+import img10 from '../images/common/img10.jpg'
+import img11 from '../images/common/img11.jpg'
+import img12 from '../images/common/img12.jpg'
+import img13 from '../images/common/img13.jpg'
+import img14 from '../images/common/img14.jpg'
+
+const dataProject = [
+	{
+		id: 1,
+		title: 'NFT #01',
+		img: img1,
+	},
+	{
+		id: 2,
+		title: 'NFT #02',
+		img: img2,
+	},
+	{
+		id: 3,
+		title: 'NFT #03',
+		img: img3,
+	},
+	{
+		id: 4,
+		title: 'NFT #04',
+		img: img4,
+	},
+	{
+		id: 5,
+		title: 'NFT #05',
+		img: img5,
+	},
+	{
+		id: 6,
+		title: 'NFT #06',
+		img: img6,
+	},
+	{
+		id: 7,
+		title: 'NFT #07',
+		img: img7,
+	},
+	{
+		id: 8,
+		title: 'NFT #08',
+		img: img2,
+	},
+
+	{
+		id: 9,
+		title: 'NFT #09',
+		img: img9,
+	},
+	{
+		id: 10,
+		title: 'NFT #10',
+		img: img10,
+	},
+	{
+		id: 11,
+		title: 'NFT #11',
+		img: img11,
+	},
+	{
+		id: 12,
+		title: 'NFT #12',
+		img: img12,
+	},
+	{
+		id: 13,
+		title: 'NFT #13',
+		img: img13,
+	},
+	{
+		id: 14,
+		title: 'NFT #14',
+		img: img14,
+	},
+	{
+		id: 15,
+		title: 'NFT #15',
+		img: img1,
+	},
+	{
+		id: 16,
+		title: 'NFT #16',
+		img: img3,
+	},
+	{
+		id: 17,
+		title: 'NFT #01',
+		img: img1,
+	},
+	{
+		id: 18,
+		title: 'NFT #02',
+		img: img2,
+	},
+	{
+		id: 19,
+		title: 'NFT #03',
+		img: img3,
+	},
+	{
+		id: 20,
+		title: 'NFT #04',
+		img: img4,
+	},
+	{
+		id: 21,
+		title: 'NFT #05',
+		img: img5,
+	},
+	{
+		id: 22,
+		title: 'NFT #06',
+		img: img6,
+	},
+	{
+		id: 23,
+		title: 'NFT #07',
+		img: img7,
+	},
+	{
+		id: 24,
+		title: 'NFT #08',
+		img: img2,
+	},
+]
+
+export default dataProject
